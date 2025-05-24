ad35a05ed092c707567298d28e2566acf546cf10
khurramdevs <khurramsaeed934@gmail.com>
2025-05-24 09:37:45 -0700

    first commit

---
+++ bin/dart_calculator_project.dart
@@ -0,0 +1,5 @@
+import 'package:dart_calculator_project/dart_calculator_project.dart' as dart_calculator_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_calculator_project.calculate()}!');
+}
---
+++ test/dart_calculator_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_calculator_project/dart_calculator_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
